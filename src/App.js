bd476f76e61dc469738874f1864ea1ed20b935d3
Filip Wachowiak <filipw01@interia.eu>
2019-09-05 20:05:04 +0200

    Display country name and color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Map from "./Map";
-import countries from './countries';
+import countries from "./countries";
+
+function translateColorToRGB(color){
+  if(color==="green"){
+    return "rgb(198, 245, 155)"
+  }
+  if(color==="yellow"){
+    return "rgb(255, 249, 121)"
+  }
+  if(color==="green-yellow"){
+    return "rgb(235, 247, 167)"
+  }
+  if(color==="red"){
+    return "rgb(245, 155, 155)"
+  }
+}
 
 function App() {
-const [selectedCountryId, setSelectedCountryId] = useState('');
+  const [selectedCountryId, setSelectedCountryId] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState();
   useEffect(() => {
-    for (const country of countries) { 
+    for (const country of countries) {
       const countryNode = document.getElementById(country.id);
-      countryNode.classList.add('hover');
-      countryNode.addEventListener('click',()=>{
+      countryNode.style.fill = translateColorToRGB(country.color);
+      countryNode.addEventListener("click", () => {
         setSelectedCountryId(country.id);
-      })
+      });
     }
   }, []);
-  const selectedCountry = countries.filter(({id})=>id === selectedCountryId);
+  useEffect(() => {
+    if (selectedCountryId) {
+      setSelectedCountry(
+        countries.filter(({ id }) => id === selectedCountryId)[0]
+      );
+    }
+  }, [selectedCountryId]);
+  console.log(selectedCountry);
   return (
     <div>
       <Map />
-      {selectedCountry.name}
+      {selectedCountry ? <div>{selectedCountry.name}</div> : ""}
     </div>
   );
 }
