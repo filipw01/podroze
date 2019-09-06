417f44a172526fd1e994db16af2cea591f646b2a
Filip Wachowiak <filipw01@interia.eu>
2019-09-06 09:04:31 +0200

    Add api

--- src/App.js
+++ src/App.js
@@ -31,11 +31,45 @@ function App() {
     }
   }, []);
   useEffect(() => {
-    if (selectedCountryId) {
-      setSelectedCountry(
-        countries.filter(({ id }) => id === selectedCountryId)[0]
-      );
+    if (selectedCountryId && selectedCountryId !== "ocean") {
+      const ourCountryProperties = countries.filter(
+        ({ id }) => id === selectedCountryId
+      )[0];
+
       document.getElementById(selectedCountryId).focus();
+      fetch(`https://restcountries.eu/rest/v2/alpha/${selectedCountryId}`)
+        .then(res => res.json())
+        .then(
+          ({
+            capital,
+            population,
+            timezones,
+            topLevelDomain,
+            languages,
+            currencies,
+            area,
+            name
+          }) => {
+            const newCountryProperties = {
+              capital,
+              population,
+              timezones: timezones ? timezones.join(", ") : "",
+              topLevelDomain,
+              languages: languages
+                ? languages.map(language => language.name)
+                : "",
+              currencies,
+              area,
+              name
+            };
+            setSelectedCountry({
+              ...newCountryProperties,
+              ...ourCountryProperties
+            });
+          }
+        );
+    } else {
+      setSelectedCountry(null);
     }
   }, [selectedCountryId]);
   function setCountry(e) {
@@ -57,13 +91,41 @@ function App() {
         <Map />
       </div>
       {selectedCountry ? (
-        <div className="country-details">{selectedCountry.name}</div>
+        <div className="country-details">
+          <p>
+            {selectedCountry.name} - {selectedCountry.color || "red"}
+          </p>
+          <p>{selectedCountry.info}</p>
+          <p>
+            Powierzchnia: {selectedCountry.area} km<sup>2</sup>
+          </p>
+          <p>Stolica: {selectedCountry.capital}</p>
+          <p>Populacja: {selectedCountry.population} ludzi</p>
+          <p>Strefy czasowe: {selectedCountry.timezones}</p>
+          <p>Domena: {selectedCountry.topLevelDomain}</p>
+          <p>Języki: {selectedCountry.languages}</p>
+          <p>
+            {selectedCountry.currencies
+              ? selectedCountry.currencies.map(currency => {
+                  return (
+                    <span key={currency.code}>
+                      {currency.code} - {currency.code} - {currency.symbol}
+                    </span>
+                  );
+                })
+              : ""}
+          </p>
+        </div>
       ) : (
         ""
       )}
       <div className="countries-list">
         {sortedAndFilteredCountries.map(({ name, id }) => {
-          return <p key={id} onClick={() => setSelectedCountryId(id)}>{name}</p>;
+          return (
+            <p key={id} onClick={() => setSelectedCountryId(id)}>
+              {name}
+            </p>
+          );
         })}
       </div>
     </div>
